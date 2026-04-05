0045f3a9693b37c0d37fb4780a45d88d2c369570
BigBarfEaterBBE <ajiang9@bostonk12.org>
2026-04-05 19:34:01 -0400

    4/5 7:33 PM ok im actually doing it on render and this is just me committing the new docs

--- ml_playground/app.py
+++ ml_playground/app.py
@@ -65,5 +65,7 @@ def augment_image():
     return jsonify({"augmented": augmented_images})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7860)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
